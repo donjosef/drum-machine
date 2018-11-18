d3eccc8e92c35f9ee824889f5acf7b363e4f5746
donjoe <giumontanaro@gmail.com>
2018-11-18 16:29:00 +0100

    Add a display component

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Power from './Power/Power';
 import SoundBank from './SoundBank/SoundBank';
-import Volume from './Volume/Volume'
+import Volume from './Volume/Volume';
+import Display from './Display/Display';
 
 const Controls = (props) => (
   <div className='controls'>
+    <Display />
     <Power />
     <SoundBank />
     <Volume />
---
+++ src/components/Controls/Display/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Display = (props) => {
+  return (
+    <div className='display'>{props.output}</div>
+
+  );
+}
+
+
+export default Display;
