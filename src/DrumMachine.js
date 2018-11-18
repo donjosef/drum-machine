2d76590c6ab9209c6ec68148afe561bd569c21c9
donjoe <giumontanaro@gmail.com>
2018-11-18 12:21:58 +0100

    Inital setup

---
+++ src/DrumMachine.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './DrumMachine.css';
+
+class DrumMachine extends Component {
+  render() {
+    return (
+      <div className="DrumMachine">
+
+        
+      </div>
+    );
+  }
+}
+
+export default DrumMachine;
