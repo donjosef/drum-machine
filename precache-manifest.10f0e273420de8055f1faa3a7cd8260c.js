1b6e5770dc4a65b3b5777d1ad73e93fbe2ec379d
donjoe <giumontanaro@gmail.com>
2018-11-20 22:17:18 +0100

    Updates

---
+++ precache-manifest.10f0e273420de8055f1faa3a7cd8260c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a991c55f20e9c7a390e86a2d6e4491",
+    "url": "/drum-machine/static/media/punchy_kick_1.a6a991c5.mp3"
+  },
+  {
+    "revision": "931f60b6777daf6a290c",
+    "url": "/drum-machine/static/css/main.2f2ec11c.chunk.css"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/drum-machine/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "6890d64eb69aba2928ee5a289ea897b3",
+    "url": "/drum-machine/static/media/pexels-photo-63703.6890d64e.jpeg"
+  },
+  {
+    "revision": "484073ffad6651504fefdb97b111ef2e",
+    "url": "/drum-machine/static/media/Bld_H1.484073ff.mp3"
+  },
+  {
+    "revision": "cd861874f961c37c2ecec27f6fd20667",
+    "url": "/drum-machine/static/media/Brk_Snr.cd861874.mp3"
+  },
+  {
+    "revision": "d8544f3e90cfa0d7ed4526b77e93408b",
+    "url": "/drum-machine/static/media/Chord_1.d8544f3e.mp3"
+  },
+  {
+    "revision": "3946f9fe73921f85ce63735196183592",
+    "url": "/drum-machine/static/media/Chord_2.3946f9fe.mp3"
+  },
+  {
+    "revision": "074c0a828b1430e030a0b5cd55ae0b3a",
+    "url": "/drum-machine/static/media/Chord_3.074c0a82.mp3"
+  },
+  {
+    "revision": "3fa8228b5b7459101ac9f7d345244b11",
+    "url": "/drum-machine/static/media/Dry_Ohh.3fa8228b.mp3"
+  },
+  {
+    "revision": "561c9a5450dd4aef32292a06f357d553",
+    "url": "/drum-machine/static/media/Give_us_a_light.561c9a54.mp3"
+  },
+  {
+    "revision": "931f60b6777daf6a290c",
+    "url": "/drum-machine/static/js/main.931f60b6.chunk.js"
+  },
+  {
+    "revision": "123a99c7ed11a3254ac92032ffba1122",
+    "url": "/drum-machine/static/media/side_stick_1.123a99c7.mp3"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drum-machine/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/drum-machine/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/drum-machine/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/drum-machine/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/drum-machine/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/drum-machine/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/drum-machine/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/drum-machine/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/drum-machine/static/media/tom.572ed39d.wav"
+  },
+  {
+    "revision": "842b96786884d2a746b6cf4d8d35f8e6",
+    "url": "/drum-machine/index.html"
+  }
+];
